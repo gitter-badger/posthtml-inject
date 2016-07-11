48fb9b49eeae55397b5bacdb3d4ab5e4b9c81904
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-07-11 23:53:10 +0200

    lib(index): updates

--- index.js
+++ index.js
@@ -8,16 +8,10 @@ module.exports = (options) => {
   options = options || {}
   options.root = options.root || './'
 
-  let head = options.head
-
   return function PostHTMLInject (tree) {
-    tree.match({tag: 'head'}, (node) => {
-      head.forEach(inject => node.content.push('\n  ', inject, '\n'))
-      return node
+    tree.match({tag: 'inject'}, (node) => {
+      console.log(node)
     })
-
-    tree.match({tag: 'inject'}, (node) => node)
-
     return tree
   }
 }
